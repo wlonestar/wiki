4064db47565afe47653c8b2acd9315d981bd1c56
wlonestar <wlonestar@163.com>
2023-10-30 13:43:28 +0800

    add example code

--- content/files/code/quick_sort.c
+++ content/files/code/quick_sort.c
@@ -3,8 +3,6 @@
 #include <assert.h>
 #include <stdlib.h>
 
-///////////////////////////////////////////////////////////////////////////////
-
 int partition(int *a, int lo, int hi) {
   int pivot = a[lo];
   while (lo < hi) {
@@ -16,33 +14,16 @@ int partition(int *a, int lo, int hi) {
   a[lo] = pivot;
   return lo;
 }
-// void _quick_sort(int *a, int lo, int hi) {
-//   if (lo < hi) {
-//     int pivot = partition(a, lo, hi);
-//     _quick_sort(a, lo, pivot - 1);
-//     _quick_sort(a, pivot + 1, hi);
-//   }
-// }
-// void quick_sort(int *a, int n) {
-//   _quick_sort(a, 0, n - 1);
-// }
-
-typedef struct Sect { int lo, hi; } Sect;
-void quick_sort(int *a, int n) {
-  int M = 32, front = 0, rear = 0;
-  Sect queue[M], p;
-  queue[rear].lo = 0; queue[rear].hi = n - 1;
-  rear = (rear + 1) % M;
-  while (front != rear) {
-    p = queue[front];
-    front = (front + 1) % M;
-    int pivot = partition(a, p.lo, p.hi);
-    queue[rear].lo = p.lo; queue[rear].hi = pivot - 1; rear = (rear + 1) % M;
-    queue[rear].lo = pivot + 1; queue[rear].hi = p.hi; rear = (rear + 1) % M;
+void _quick_sort(int *a, int lo, int hi) {
+  if (lo < hi) {
+    int pivot = partition(a, lo, hi);
+    _quick_sort(a, lo, pivot - 1);
+    _quick_sort(a, pivot + 1, hi);
   }
 }
-
-///////////////////////////////////////////////////////////////////////////////
+void quick_sort(int *a, int n) {
+  _quick_sort(a, 0, n - 1);
+}
 
 int main(int argc, char *argv[]) {
   assert(argc == 2);
